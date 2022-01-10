258c437673df03fb49cd1c12864f80835bc3ff99
shfosem <37168972+shfosem@users.noreply.github.com>
2022-01-10 10:03:00 +0900

    Sign Server prorject
    사인 서버 재구축해서 추가합니다.
    bulid/libs/signserver-0.0.1-SNAPSHOT을 
    java -jar로 실행하셔도 되고,
    intellij에서 프로젝트 통째로 열어서 SignserverApplication 실행하셔도 서버 돌아갑니다.
    
    resource/static에 상민이형이 구현한 프론트 파일들 넣어두었고,
    Spring 에서 정적 컨텐츠를 default 로 찾아서 여는 이름이 index.html이여서 
    main.html -> index.html 로 이름만 바꾸었습니다.

---
+++ signserver/src/test/java/jhs/signserver/SignserverApplicationTests.java
@@ -0,0 +1,13 @@
+package jhs.signserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
