ff06a55624f3d8d4ee67edb44ac76700f55bc419
shfosem <37168972+shfosem@users.noreply.github.com>
2022-01-18 17:02:22 +0900

    JPA 연결을 위한 기본 틀
    매핑을 위한 Entity 어노테이션 추가하고.. 필요한 기본적인 것들 추가하였습니다.
    현재 서버 실행하면, driver class를 load하지 못한다고 서버가 실행되지 않는데.. 필요한 드라이버 뭐 설치해야되는지 찾아보고 빠른 시일내에 조치하도록 하겠습니다.
    
    서버 돌아가게 하려면, build.gradle 에서 추가한 부분 다시 주석처리하고 , application.properties 도 이전 버전으로 수정해야될 거에여~

---
+++ signserver/src/main/java/jhs/signserver/SpringConfig.java
@@ -0,0 +1,5 @@
+package jhs.signserver;
+
+public class SpringConfig {
+    //현재 Annotation으로 연결된 것을 여기서 한번에 연결되도록 짜야될 수도 있음..
+}
--- signserver/src/main/java/jhs/signserver/service/SignService.java
+++ signserver/src/main/java/jhs/signserver/service/SignService.java
@@ -6,9 +6,10 @@ import jhs.signserver.repository.SignRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-
+@Transactional
 @Service
 public class SignService {
 
