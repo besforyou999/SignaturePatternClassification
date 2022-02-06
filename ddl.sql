d63599bb68b8dd028ae10616a1fbefb478100b6b
Sangmin Ju <besforyou2@gmail.com>
2022-02-06 18:29:23 +0900

    delete unecessary files

--- ddl.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `sign` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `data` longblob DEFAULT NULL,
-  `image` varchar(2083) DEFAULT NULL,
-  `label` int(11) DEFAULT NULL,
-  `created` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
