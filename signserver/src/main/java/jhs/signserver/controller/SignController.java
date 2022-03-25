de67f36ddb5f85bd354099049e6d38a5489e15c0
shfosem <ssh9806@naver.com>
2022-03-25 15:14:24 +0900

    send image mapping

--- signserver/src/main/java/jhs/signserver/controller/SignController.java
+++ signserver/src/main/java/jhs/signserver/controller/SignController.java
@@ -116,6 +116,48 @@ public class SignController {
         return "redirect:/";
     }
 
+    @RequestMapping("/sendImage")
+    public String sendImage(@RequestParam(value="file") MultipartFile [] file ) {
+
+        byte[] bytes = null;
+        try {
+            bytes = file[0].getBytes();
+        } catch( Exception e) {
+            System.out.println("get bytes error");
+            return "redirect:/";
+        }
+        Sign sign = new Sign();
+        // sign.setImage(Base64.getEncoder().encode(bytes));
+        String s = Base64.getEncoder().encodeToString(bytes);
+
+        s = "data:image/png;base64," + s;
+
+        String number = null;
+        try {
+            number = new String(file[1].getBytes());
+        } catch (Exception e) {
+            System.out.println("number exception");
+            return "redirect:/";
+        }
+
+
+        // img URL
+        sign.setData(s);
+        // Creation date
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String ss = sdf.format(new java.util.Date());
+        sign.setCreated(java.sql.Date.valueOf(ss));
+
+        // img type
+        sign.setLabel(Integer.parseInt(number));
+
+        /*
+        이거 대신 소켓 통신으로 전달!
+        signService.register(sign);*/
+
+        return "redirect:/";
+    }
+
     @RequestMapping("/deleteSign")
     public String deleteImage(@RequestParam(value="id") String id, Model model) throws Exception{
         Sign sign = signService.findOne(Long.parseLong(id)).get();
