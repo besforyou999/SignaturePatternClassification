c5452893adf3d707c626009448bdcd717dab2c39
Sangmin <besforyou2@gmail.com>
2022-03-25 11:57:22 +0900

    add main screen to select store data, test model

--- signserver/src/main/java/jhs/signserver/controller/MainController.java
+++ signserver/src/main/java/jhs/signserver/controller/MainController.java
@@ -1,15 +1,23 @@
 package jhs.signserver.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String home(){
+        return "main";
+    }
+
+    @GetMapping("/newData")
+    public String createDataPage() {
         return "newData";
+    }
+
+    @GetMapping("/sendData")
+    public String sendDataPage() {
+        return "sendData";
     }
 }
