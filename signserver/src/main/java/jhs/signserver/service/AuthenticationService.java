6e51f3b6e8a3ae8bcc91dd9fba2c608eb128e005
sGOM <pooh6195@naver.com>
2022-05-23 02:11:02 +0900

    인증 데이터 전송 추가 (SignController.java , AuthenticationService.java , confirmData.js 수정)

--- signserver/src/main/java/jhs/signserver/service/AuthenticationService.java
+++ signserver/src/main/java/jhs/signserver/service/AuthenticationService.java
@@ -51,4 +51,43 @@ public class AuthenticationService {
             e.printStackTrace();
         }
     }
+
+
+    public void confirmSign(String encoded_str){
+        try (Socket client = new Socket()) {
+            InetSocketAddress ipep = new InetSocketAddress("", 9980);
+
+            client.connect(ipep);
+
+            try (OutputStream sender = client.getOutputStream(); InputStream receiver = client.getInputStream();) {
+
+                byte[] data = encoded_str.getBytes();
+                ByteBuffer b = ByteBuffer.allocate(4);
+                b.order(ByteOrder.LITTLE_ENDIAN);
+                b.putInt(data.length);
+
+                sender.write(b.array(), 0, 4);
+                sender.write(data);
+
+                data = new byte[4];
+
+                receiver.read(data, 0, 4);
+
+                //ByteBuffer
+                b = ByteBuffer.wrap(data);
+                b.order(ByteOrder.LITTLE_ENDIAN);
+                int length = b.getInt();
+
+                data = new byte[length];
+
+                receiver.read(data, 0, length);
+
+                encoded_str = new String(data, "UTF-8");
+
+
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
 }
